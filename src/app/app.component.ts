b7e9026da2ca27f030149930f9fbc4a2950640d8
Zong Yuan Lee <zy.lee@nettium.net>
2021-08-28 22:42:35 +0800

    sidebar design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  status: boolean = false;
   title = 'Travelling-Planning';
+
+  clickEvent(){
+    this.status = !this.status;
+  }
 }
